c28098764ad3aef36d6b2d549cb5c689d23086ba
Brown1ie <edward.a.brown@icloud.com>
2024-12-23 13:28:26 +0000

    Deployment Fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/JobScraper/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
